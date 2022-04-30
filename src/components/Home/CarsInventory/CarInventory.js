0054ee0a0ceb9dce8ab17005217462c9c50d5605
ImranHossain5722 <imd572258@gmail.com>
2022-04-30 15:23:27 +0600

    loading api working

--- src/components/Home/CarsInventory/CarInventory.js
+++ src/components/Home/CarsInventory/CarInventory.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const CarInventory = () => {
 
-  const [inventoryItems, setInventoryItems]=useState()
+  const [inventoryItems , setInventoryItems]= useState([])
 
-  user
+  useEffect(()=>{
+   
+    fetch ('https://rocky-dawn-40302.herokuapp.com/inventoryItems')
+    .then (res => res.json)
+    .then (data => setInventoryItems(data))
+
+  },[])
 
   return (
-    <div>
-      <div>
+    <div className="container">
+      <div className="row">
           <h1 className="text-center">Our Cars in stock</h1>
           <div style={{ height: "3px" , width:"50px" }} className="bg-primary  mx-auto "></div>
 
-          <div>
-
+          <div >
+              <h1>inventory Items:{inventoryItems.length} </h1>
           </div>
       </div>
     </div>
