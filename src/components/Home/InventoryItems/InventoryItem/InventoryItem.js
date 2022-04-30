732a58294502f73df2b95b1779c79efa6723e21f
ImranHossain5722 <imd572258@gmail.com>
2022-04-30 17:09:44 +0600

    build manage stcok button

--- src/components/Home/InventoryItems/InventoryItem/InventoryItem.js
+++ src/components/Home/InventoryItems/InventoryItem/InventoryItem.js
@@ -8,14 +8,14 @@ const InventoryItem = ({ inventoryItem }) => {
     <div>
       <div className="card mx-2 inventory-item-div mt-5">
           <img className="card-img-top" src={img} alt=""/>
-          <div className="card-body">
+          <div className="card-body p-0">
           <p><span className="inventory-text" >Supplier Name :</span> {supplierName}</p>
           <p><span className="inventory-text" >Car Name : </span>{name}</p>
           <p><span className="inventory-text" >Brand:</span> {brand}</p>
           <p><span className="inventory-text" >Quantity : </span>{quantity}</p>
           <p><span className="inventory-text" >Price : </span>{price}</p>
           <p><span className="inventory-text" >Short Detail :</span> {description}</p>
-           <button className="btn-mange-stock" >Manage Stock</button>
+           <button className="btn-mange-stock d-flx justify-content-center" >Manage Stock</button>
           </div>
       </div>
     </div>
