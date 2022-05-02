eea78c96bc71f6c7a11cf408f5e376871d21a615
ImranHossain5722 <imd572258@gmail.com>
2022-05-02 15:59:25 +0600

    add fontAwsome icon for delete and addNewitems

--- src/components/Manage Items/ManageSingleItem/ManageSingleItem.js
+++ src/components/Manage Items/ManageSingleItem/ManageSingleItem.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./ManageSingleItem.css";
+import { faCirclePlus, faTrash,  } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const ManageSingleItem = ({ useInventoryItem }) => {
   const { name, img, price, brand, description, quantity, supplierName } =
     useInventoryItem;
   return (
-    <div className="main-items-container w-50 mx-auto mt-5 align-items center">
+    <div className="main-items-container w-50 mx-auto mt-5 p- align-items center">
       <div className="items-img">
-        <img src={img} alt="" />
+        <img className="figure-img img-fluid rounded" src={img} alt="" />
       </div>
 
       <div className=" items-container">
         <div className="items-details">
-          <p> <span>Name:</span> {name}</p>
-          <p> <span>Brand:</span> {brand}</p>
-          <p> <span>Quantity:</span> {quantity}</p>
-          <p> <span>Supplier Name:</span> {supplierName}</p>
-          <p> <span>Price :</span> {price}</p>
+          <p> <span className="items-detail">Name:</span> {name}</p>
+          <p> <span className="items-detail">Brand:</span> {brand}</p>
+          <p> <span className="items-detail">Quantity:</span> {quantity}</p>
+          <p> <span className="items-detail">Supplier Name:</span> {supplierName}</p>
+          <p> <span className="items-detail">Price :</span> {price}</p>
         </div>
         <div className="delete-btn-container">
-          <button className="btn"> X </button>
+          <button className="btn"> <FontAwesomeIcon className="deleteIcon" icon={faTrash} ></FontAwesomeIcon> </button>
+          <button className="btn"> <FontAwesomeIcon className="deleteIcon" icon={faCirclePlus} ></FontAwesomeIcon> </button>
         </div>
       </div>
     </div>
