f50136351ec3bb7cdaafe340b3d0d63ea5673b29
ImranHossain5722 <imd572258@gmail.com>
2022-05-07 05:26:37 +0600

    blog page

--- src/components/Blogs/Blog/Blog.js
+++ src/components/Blogs/Blog/Blog.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import './Blog.css'
 const Blog = ({SingleBlog}) => {
     
     const {title, blog,admin, img, _id}= SingleBlog
     const navigate = useNavigate();
     return (
         <div>
-        <div className="card mx-3 mt-5 p-2">
+        <div className="card blog-wrap mx-3 mt-5 p-2">
           <img className="card-img-top p-2 " height="200" src={img} alt=""/>
           <div className="card-body p-">
           <p className='text-left'> Author: {admin}</p>
